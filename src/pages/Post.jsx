ab291eea3ec1487ebcf184110b5e17f8b00e97ba
sharon <sharonyak15@gmail.com>
2022-09-01 08:42:20 +0000

    Update Post.jsx and styles.css

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,7 +4,7 @@ const Post = ({postList}) => {
     const {id} = useParams()
     const post = postList.filter(post => post.id === id)
     return (
-        <div>
+        <div className="singlePost">
             <h1>{post[0].title}</h1>
             <h3>Written by {post[0].author.name} </h3>
             <p>{post[0].postText}</p>
